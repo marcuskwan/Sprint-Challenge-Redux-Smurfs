aa8c1461508bf5e76abb02f33697cb6c97e86a78
Marcus Kwan <kwan.marcus@gmail.com>
2019-06-22 01:30:33 +1000

    imported rootreducer, built out basic reducer, used thunk and logger

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -21,3 +21,10 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+export const rootReducer = (initialState = state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
